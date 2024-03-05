5edca53fd0f7bc972dc9e2e899b6d651dfc05dbf
Ali-Imran01 <aliimranrohaizi.air@gmail.com>
2024-03-05 18:32:30 +0800

    update

---
+++ tutorial.php
@@ -0,0 +1,54 @@
+<?php
+
+define('NAME', 'Ali');
+// $name = 'ali';
+$age = 23;
+
+$name = 'mario';
+
+// $stringOne = 'My email is ';
+$stringTwo = 'Ali@Test.com';
+
+// echo $stringOne . $stringTwo;
+
+// echo 'hey my name is ' . $name;
+
+// echo "hey, my name is $name";
+
+// echo "$name screamed \" WAAHHHH!!\"";
+
+// echo NAME . ' screamed "WAAHHHH!"';
+
+// echo $name[2];
+
+// count the total of the string
+// echo strlen($stringTwo);
+
+// turn all the strings to upper case
+// echo strtoupper($stringTwo);
+
+// turn strings to lower case
+// echo strtolower($stringTwo);
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coookies</title>
+</head>
+
+<body>
+
+    <h1><?php echo NAME; ?></h1>
+    <h1><?php echo $age; ?></h1>
+
+
+</body>
+
+</html>
